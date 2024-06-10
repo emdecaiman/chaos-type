28b75e958cddfa4c7d949c71bc0f9df17f713ef4
Emmanuel de Caiman <emdecaiman@gmail.com>
2024-06-10 00:34:39 -0700

    implemented input and check

--- src/game-components/Game.jsx
+++ src/game-components/Game.jsx
@@ -21,11 +21,12 @@ const List = (props) => {
 
 const Game = () => {
     const [wordList, setWordList] = useState([]);
+    const [input, setInput] = useState("");
     
     useEffect(() => {
         const id = setInterval(() => {
             addWord();
-        }, 2000);
+        }, 750);
 
         return () => clearInterval(id);
     }, [wordList]); // interval resets everytime wordList changes
@@ -40,13 +41,38 @@ const Game = () => {
         setWordList(newWordList);
     }
 
+    const checkInputIsValid = (event) => {
+        if (event.key === "Enter") {
+            if (wordList.some(word => word.text === input)) {
+                console.log("Input in wordlist")
+                removeWord(input);
+                setInput("");
+            } else {
+                console.log("Input not in wordlist");
+            }
+        }
+    }
+
+    const removeWord = (wordToRemove) => {
+        const updatedWordList = wordList.filter(word => word.text !== wordToRemove);
+        setWordList(updatedWordList);
+    }
+
     return (
-        <div className="flex flex-row justify-center h-full m-20">
-            <div className="bg-green-500 w-3/4">
-                <List wordList={wordList} />
+        <>
+            <div className="flex flex-row justify-center h-full m-20">
+                <div className="bg-green-500 w-3/4">
+                    <List wordList={wordList} />
+                </div>
+                <Stats/>
             </div>
-            <Stats/>
-        </div>
+            <input 
+                value={input} 
+                onChange={e => setInput(e.target.value.trim())} 
+                onKeyDown={checkInputIsValid} 
+                type="text" 
+            />
+        </>
     );
 }
 
