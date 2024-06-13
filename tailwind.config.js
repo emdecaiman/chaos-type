429b5ea566b27a5db87c0bd769c3e4b2a7e72bb0
Emmanuel de Caiman <emdecaiman@gmail.com>
2024-06-12 23:04:16 -0700

    included tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,23 @@ export default {
       fontFamily: {
         sourcecodepro: ["Source Code Pro", "sans-serif"],
       },
+      // https://www.30secondsofcode.org/css/s/zoomin-zoomout-animation/
+      animation: {
+        zoominout: "zoominout 5s ease infinite",
+      },
+      keyframes: {
+        zoominout: {
+          "0%": {
+            transform: "scale(0.2)",
+          },
+          "50%": {
+            transform: "scale(1)",
+          },
+          "100%": {
+            transform: "scale(0.2)",
+          },
+        }
+      }
     },
   },
   plugins: [],
